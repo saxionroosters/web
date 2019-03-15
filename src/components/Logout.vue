6b4a488ff8284504747e030cd2f3e720b345efab
Rutger Nijhuis <rutger.nijhuis@gmail.com>
2019-03-15 14:09:02 +0100

    added redirect

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -2,12 +2,21 @@
     <v-flex>
         <h1 class="title">Uitgelogd</h1>
         <p>Je bent nu uitgelogd.</p>
+        <p>Je wordt zo doorverwezen.</p>
     </v-flex>
 </template>
 
 <script>
     export default {
-        name: "Logout"
+        name: "Logout",
+        mounted() {
+            localStorage.removeItem('access_token')
+
+            setTimeout(() => {
+                this.$router.push('Login')
+            }, 5000)
+
+        }
     }
 </script>
 
