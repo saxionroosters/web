0381c90c2ee94e052b77fec22d1e6a2b09dee8a5
wesselperik <wessel@perik.me>
2019-03-21 19:50:30 +0100

    Styling fixes

--- src/components/interface/Toolbar.vue
+++ src/components/interface/Toolbar.vue
@@ -1,66 +1,72 @@
 <template>
-    <v-toolbar color="theme-primary" dark app :clipped-left="$vuetify.breakpoint.mdAndUp" fixed>
-        <v-toolbar-title class>
-            <v-toolbar-side-icon @click.stop="$emit('update:drawer', !drawer)"></v-toolbar-side-icon>
-            <span class="logo hidden-sm-and-down">
+  <v-toolbar color="theme-primary" dark app :clipped-left="$vuetify.breakpoint.mdAndUp" fixed>
+    <v-toolbar-title class>
+      <v-toolbar-side-icon @click.stop="$emit('update:drawer', !drawer)"></v-toolbar-side-icon>
+      <span class="logo hidden-sm-and-down">
         <span>saxion</span>roosters
       </span>
-        </v-toolbar-title>
-        <v-spacer class="hidden-sm-and-down"></v-spacer>
+    </v-toolbar-title>
+    <v-spacer class="hidden-sm-and-down"></v-spacer>
 
-        <v-text-field flat solo-inverted prepend-inner-icon="search"
-                      label="Search"
-                      zclass="toolbar-search"
-                      v-model="this.searchQuery"
+    <v-text-field
+      flat
+      solo-inverted
+      prepend-inner-icon="search"
+      label="Search"
+      zclass="toolbar-search"
+      v-model="searchQuery"
+    ></v-text-field>
 
-        ></v-text-field>
+    <v-menu bottom left>
+      <v-btn slot="activator" icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
 
-        <v-menu bottom left>
-            <v-btn slot="activator" icon>
-                <v-icon>more_vert</v-icon>
-            </v-btn>
+      <v-list style="min-width: 150px;">
+        <router-link tag="v-list-tile" v-bind:to="{ name: 'Home' }">
+          <v-list-tile-title>{{ $t('titles.about') }}</v-list-tile-title>
+        </router-link>
 
-            <v-list style="min-width: 150px;">
-                <router-link tag="v-list-tile" v-bind:to="{ name: 'Home' }">
-                    <v-list-tile-title>{{ $t('titles.about') }}</v-list-tile-title>
-                </router-link>
+        <router-link tag="v-list-tile" v-bind:to="{ name: 'Home' }">
+          <v-list-tile-title>{{ $t('titles.apps') }}</v-list-tile-title>
+        </router-link>
 
-                <router-link tag="v-list-tile" v-bind:to="{ name: 'Home' }">
-                    <v-list-tile-title>{{ $t('titles.apps') }}</v-list-tile-title>
-                </router-link>
+        <v-divider/>
 
-                <v-divider/>
-
-                <router-link tag="v-list-tile" v-bind:to="{ name: 'Logout' }">
-                    <v-list-tile-title>{{ $t('titles.logout') }}</v-list-tile-title>
-                </router-link>
-            </v-list>
-        </v-menu>
-    </v-toolbar>
+        <router-link tag="v-list-tile" v-bind:to="{ name: 'Logout' }">
+          <v-list-tile-title>{{ $t('titles.logout') }}</v-list-tile-title>
+        </router-link>
+      </v-list>
+    </v-menu>
+  </v-toolbar>
 </template>
 <script>
-    export default {
-        props: ["drawer"],
-        methods: {
-            search: function (sender) {
-                this.$router.replace({name: 'Search', params: {q: sender}})
-            }
-
-        },
-        data() {
-            return {
-                searchQuery: null
-            }
-        },
-        mounted() {
-            let searchParam = this.$route.query.q;
-            if (searchParam != null) {
-                this.searchQuery = searchParam
-            }
-        },
+export default {
+  props: ["drawer"],
+  methods: {
+    search: function(sender) {
+      this.$router.replace({ name: "Search", params: { q: sender } });
     }
+  },
+  data() {
+    return {
+      searchQuery: null
+    };
+  },
+  mounted() {
+    let searchParam = this.$route.query.q;
+    if (searchParam != null) {
+      this.searchQuery = searchParam;
+    }
+  },
+  watch: {
+      searchQuery: function(val) {
+          this.$router.replace({ name: "Search", params: { q: val } });
+      }
+  }
+};
 </script>
 
 <style lang="scss">
-    /** loaded from scss folder **/
+/** loaded from scss folder **/
 </style>
--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,69 +1,64 @@
 <template>
-    <v-layout row>
+  <v-layout row>
+    <v-flex>
+      <v-list two-line>
+        <template v-if="this.results.groups.length > 0">
+          <v-subheader>Groepen</v-subheader>
+          <v-divider></v-divider>
+          <v-list-tile v-for="group in this.results.groups" ripple @click="openGroup(group.name)">
+            <v-list-tile-content>
+              <v-list-tile-title v-html="group.name"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="group.course_name"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
 
-        <v-flex>
-            <v-list two-line>
-
-                <template v-if="this.results.groups.length > 0">
-                    <v-subheader>
-                        Groepen
-                    </v-subheader>
-                    <v-divider></v-divider>
-                    <v-list-tile v-for="group in this.results.groups" ripple @click="openGroup(group.name)">
-                        <v-list-tile-content>
-                            <v-list-tile-title v-html="group.name"></v-list-tile-title>
-                            <v-list-tile-sub-title v-html="group.course_name"></v-list-tile-sub-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-
-                <template v-if="this.results.teachers.length > 0">
-                    <v-subheader>
-                        Leraren
-                    </v-subheader>
-                    <v-divider></v-divider>
-                    <v-list-tile v-for="teacher in this.results.teachers" ripple @click="openTeacher(teacher.code)">
-                        <v-list-tile-content>
-                            <v-list-tile-title v-html="teacher.fullname"></v-list-tile-title>
-                            <v-list-tile-sub-title v-html="teacher.code"></v-list-tile-sub-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-
-            </v-list>
-        </v-flex>
-
-    </v-layout>
+        <template v-if="this.results.teachers.length > 0">
+          <v-subheader>Leraren</v-subheader>
+          <v-divider></v-divider>
+          <v-list-tile
+            v-for="teacher in this.results.teachers"
+            ripple
+            @click="openTeacher(teacher.code)"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title v-html="teacher.fullname"></v-list-tile-title>
+              <v-list-tile-sub-title v-html="teacher.code"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-    import ScheduleManager from "../../managers/ScheduleManager";
+import ScheduleManager from "../../managers/ScheduleManager";
 
-    export default {
-        name: "Search",
-        data() {
-            return {
-                results: null
-            }
-        },
-        mounted() {
-            let searchQuery = this.$route.query.q
-            new ScheduleManager().search(searchQuery, (result) => {
-                this.results = result.result
-                console.log(this.results)
-            })
-        },
-        methods: {
-            openGroup: function(groupId){
-                this.$router.push({name: 'Schedule', query: {group: groupId}})
-            },
-            openTeacher: function(teacherId){
-                this.$router.push({name: 'Schedule', query: {teacher: teacherId}})
-            }
-        }
+export default {
+  name: "Search",
+  data() {
+    return {
+      results: null
+    };
+  },
+  mounted() {
+    let searchQuery = this.$route.query.q;
+    new ScheduleManager().search(searchQuery, result => {
+      this.results = result.result;
+      console.log(this.results);
+    });
+  },
+  methods: {
+    openGroup: function(groupId) {
+      this.$router.push({ name: "Schedule", query: { group: groupId } });
+    },
+    openTeacher: function(teacherId) {
+      this.$router.push({ name: "Schedule", query: { teacher: teacherId } });
     }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
